174f9119142a09700a7bb12d0a5b49fe724af3f5
Juan Correa <juan.correa.herrera@gmail.com>
2023-09-11 19:44:21 -0600

    updated vite config base

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/advanced-react-patterns/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
